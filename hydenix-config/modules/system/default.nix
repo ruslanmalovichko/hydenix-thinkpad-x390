47a3a0fd25b32b9e5737347e834d5ec1993ebd6e
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2026-02-17 02:22:35 +0200

    feat: Change hydenix configs

--- hydenix-config/modules/system/default.nix
+++ hydenix-config/modules/system/default.nix
@@ -45,10 +45,19 @@
     tree-sitter
     dotnet-sdk
     omnisharp-roslyn
+    # gnome-keyring
+    # libsecret
+    google-chrome
+    rpi-imager
+    realvnc-vnc-viewer
   ];
 
   environment.sessionVariables = {
     DOTNET_ROOT = "${pkgs.dotnet-sdk_8}/share/dotnet";
+  };
+
+  environment.variables = {
+    EDITOR = "nvim";
   };
 
   systemd.services.warp-svc = {
@@ -83,10 +92,43 @@
 
   zramSwap.enable = true;
   zramSwap.algorithm = "zstd";
-  zramSwap.memoryPercent = 75;
+  zramSwap.memoryPercent = 90;
   zramSwap.priority = 100;
 
   boot.kernel.sysctl = {
     "vm.swappiness" = 150;
+  };
+
+  networking.extraHosts = ''
+    127.0.0.1 local.ptprocover.com
+  '';
+
+  # services.gnome-keyring = {
+  #   enable = true;
+  #   components = [ "pkcs11" "secrets" "ssh" ];
+  # };
+
+  services.gnome.gnome-keyring.enable = true;
+
+  # boot.kernelParams = [
+  #   "battery.charge_control_end_threshold=80"
+  #   "battery.charge_control_start_threshold=75"
+  # ];
+
+  # services.udev.extraRules = ''
+  #   SUBSYSTEM=="power_supply", ATTR{charge_control_end_threshold}="80"
+  #   SUBSYSTEM=="power_supply", ATTR{charge_control_start_threshold}="75"
+  # '';
+
+  systemd.services.battery-charge-thresholds = {
+    description = "Set battery charge thresholds";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = [
+        "/bin/sh -c 'echo 80 > /sys/class/power_supply/BAT0/charge_control_end_threshold'"
+        "/bin/sh -c 'echo 75 > /sys/class/power_supply/BAT0/charge_control_start_threshold'"
+      ];
+    };
   };
 }
